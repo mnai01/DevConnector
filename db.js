b45633b97f9fcfbbfc7ebbd4360939a1815c7ad1
mnai01 <imatlak03@gmail.com>
2020-05-24 03:24:49 -0400

    added connection to mongodb atlas

---
+++ db.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+// packed used to get info from a config file
+const config = require('config');
+// holds our key
+const db = config.get('mongoURI');
+
+// returns a promise
+// using an async await
+// most of the time async await functions require a try catch
+const connectDB = async () => {
+  try {
+    // since this returns a promise we want to put await here
+    // the code after the await function will get executed
+    // once the promise is returned
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Mongo DB connected');
+  } catch (err) {
+    console.log(err.message);
+    // Exit the process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
