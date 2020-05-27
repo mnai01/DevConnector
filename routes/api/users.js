9f8afde27c9406beea9fceeacb8ad55aeff3507c
mnai01 <imatlak03@gmail.com>
2020-05-26 22:22:08 -0400

    Added validating user registration data

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register user
 // @access  Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    // checks to make sure its is not empty
+    check('name', 'Name is required').not().isEmpty(),
+    // checks to make sure its an email
+    check('email', 'Email is required').isEmail(),
+    // checks to make sure it contains min characters
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    // takes the users req and checks it with validationResult function
+    // if any of the data doesnt match the specifed rules above "const errors" will contain error data
+    const errors = validationResult(req);
+    // if errors is not empty
+    if (!errors.isEmpty()) {
+      console.log('ERROR VALIDATING USER REGISTRATION');
+      // return a 400 status and json object of the errors.array which contains the messages
+      return res.status(400).json({ errors: errors.array() });
+    }
+    console.log('SUCESSFULLY REGISTERED USER');
+    console.log(req.body);
+    res.send('User route');
+  }
+);
 
 module.exports = router;
