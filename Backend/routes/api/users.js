497bec409417ccf717ce1d1adda981a0d1fe0e64
Ian <imatlak03@gmail.com>
2020-10-19 12:00:57 -0400

    added Controller

--- Backend/routes/api/users.js
+++ Backend/routes/api/users.js
@@ -1,29 +1,30 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const gravatar = require("gravatar");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const config = require("config");
-const { check, validationResult } = require("express-validator");
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
-const { registerUser } = require("../../controllers/usersController");
+const { registerUser } = require('../../controllers/usersController');
 
 // User Model
-const User = require("../../models/Users");
+const User = require('../../models/Users');
 
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
-router.post("/", [
+router.post('/', [
   // checks to make sure its is not empty
-  check("name", "Name is required").not().isEmpty(),
+  check('name', 'Name is required').not().isEmpty(),
   // checks to make sure its an email
-  check("email", "Email is required").isEmail(),
+  check('email', 'Email is required').isEmail(),
   // checks to make sure it contains min characters
   check(
-    "password",
-    "Please enter a password with 6 or more characters"
+    'password',
+    'Please enter a password with 6 or more characters'
   ).isLength({ min: 6 }),
+  registerUser,
 ]);
 
 module.exports = router;
