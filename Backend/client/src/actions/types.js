8468cb48982ae9a995a4c099f94028ae3ca5d78c
Ian <imatlak03@gmail.com>
2021-02-12 01:10:49 -0500

    updated auth reducer

--- Backend/client/src/actions/types.js
+++ Backend/client/src/actions/types.js
@@ -2,5 +2,9 @@
 // Also it is a centralized file with all our action types
 export const SET_ALERT = 'SET ALERT';
 export const REMOVE_ALERT = 'REMOVE ALERT';
-export const REGISTER_SUCESS = 'REGISTER_SUCESS';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
--- Backend/client/src/reducers/auth.js
+++ Backend/client/src/reducers/auth.js
@@ -1,4 +1,11 @@
-import { REGISTER_FAIL, REGISTER_SUCESS } from '../actions/types';
+import {
+  REGISTER_FAIL,
+  REGISTER_SUCCESS,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+} from '../actions/types';
 
 const initialState = {
   token: localStorage.getItem('token'),
@@ -8,17 +15,27 @@ const initialState = {
 };
 
 export default function (state = initialState, action) {
+  // payload will represent the users data
   const { type, payload } = action;
   switch (type) {
-    case REGISTER_SUCESS:
-      // add token to storage
+    case USER_LOADED:
+      return { ...state, user: payload, isAuthenticated: true, loading: false };
+
+    // These 2 types do the same thing
+    case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', payload.token);
       return { ...state, ...payload, isAuthenticated: true, loading: false };
+
+    // These 3 types do the same thing
     case REGISTER_FAIL:
+    case LOGIN_FAIL:
+    case AUTH_ERROR:
       // remove token from storage
       localStorage.removeItem('token');
       // set value to null
-      return { ...state, payload, isAuthenticated: false, loading: false };
+      return { ...state, token: null, isAuthenticated: false, loading: false };
+
     default:
       return state;
   }
