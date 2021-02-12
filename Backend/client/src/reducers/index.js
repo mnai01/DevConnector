41cb5ca6be3c21f1a6b31db8b65fb80767198f0e
Ian <imatlak03@gmail.com>
2021-02-12 01:09:34 -0500

    comments

--- Backend/client/src/reducers/index.js
+++ Backend/client/src/reducers/index.js
@@ -3,6 +3,6 @@ import alert from './alert';
 import auth from './auth';
 
 // takes all the reducers and combines then. Then when you use getStore it calls every reducer and returns the values frome each in an object
-
+// each state will be help is the reducer with an obj name of it self
 console.log('IN REDUCER INDEX');
 export default combineReducers({ alert, auth });
