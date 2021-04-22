eebe4a5d10419d012274392e78db0fda5ddbdc67
Ian <imatlak03@gmail.com>
2021-04-22 01:30:51 -0400

    get logged in users profile

--- Backend/client/src/reducers/index.js
+++ Backend/client/src/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
+import profile from './profile';
 
 // takes all the reducers and combines then. Then when you use getStore it calls every reducer and returns the values frome each in an object
 // each state will be help is the reducer with an obj name of it self
 console.log('IN REDUCER INDEX');
-export default combineReducers({ alert, auth });
+export default combineReducers({ alert, auth, profile });
